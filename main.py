7568986ba64ded7e2a61ad9517afdec23efc12e3
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2017-01-19 14:24:22 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-#constants
+# constants
 SERVO_1 = 11
 SERVO_2 = 13
 CLOSED_FREQ = 10
@@ -10,17 +10,30 @@ OPEN_CCW = 0.8
 OPEN_CW = 2.1
 WAIT_TIME = 1
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(SERVO_1,GPIO.OUT)
-GPIO.setup(SERVO_2, GPIO.OUT)
-pwm_1 = GPIO.PWM(SERVO_1, CLOSED_FREQ)
-pwm_2 = GPIO.PWM(SERVO_2, CLOSED_FREQ)
-pwm_1.start(CLOSED_CDC)
-pwm_2.start(CLOSED_CDC)
-while True:
-    time.sleep(WAIT_TIME)
-    pwm_1.ChangeDutyCycle(OPEN_CCW)
-    pwm_2.ChangeDutyCycle(OPEN_CW)
-    time.sleep(WAIT_TIME)
-    pwm_1.ChangeDutyCycle(CLOSED_CDC)
-    pwm_2.ChangeDutyCycle(CLOSED_CDC)
+# globals
+door_1 = None
+door_2 = None
+
+
+def init():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(SERVO_1, GPIO.OUT)
+    GPIO.setup(SERVO_2, GPIO.OUT)
+    global door_1
+    global door_2
+    door_1 = GPIO.PWM(SERVO_1, CLOSED_FREQ)
+    door_2 = GPIO.PWM(SERVO_2, CLOSED_FREQ)
+    door_1.start(CLOSED_CDC)
+    door_2.start(CLOSED_CDC)
+
+
+def main():
+    init()
+    while True:
+        time.sleep(WAIT_TIME)
+        door_1.ChangeDutyCycle(OPEN_CCW)
+        door_2.ChangeDutyCycle(OPEN_CW)
+        time.sleep(WAIT_TIME)
+        door_1.ChangeDutyCycle(CLOSED_CDC)
+        door_2.ChangeDutyCycle(CLOSED_CDC)
+main()
