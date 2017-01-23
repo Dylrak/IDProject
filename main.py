dbf0d29f734f058ca1f5bf3b8daeb6a81b0f16b1
Dylrak-Pi <dylrak@hotmail.com>
2017-01-23 12:24:33 +0000

    Added file to support usage of RFID-reader

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
 import RPi.GPIO as GPIO
 import time
 import MFRC522
@@ -10,13 +13,13 @@ CLOSED_FREQ = 10
 CLOSED_CDC = 1.5
 OPEN_CCW = 0.8
 OPEN_CW = 2.1
-WAIT_TIME = 1
+NOCHANGE = 0
+WAIT_TIME = 5
 
 exitProcess = False
 
 
-class GateProcess:
-    # Process to open and close gates
+class GateProcess:  # Process to open and close gates
     # global within gateProcess
     door_1 = None
     door_2 = None
@@ -36,10 +39,8 @@ class GateProcess:
             self.openDoors()
 
     def openDoors(self):
-        time.sleep(WAIT_TIME)
         door_1.ChangeDutyCycle(OPEN_CCW)
         door_2.ChangeDutyCycle(OPEN_CW)
-        time.sleep(WAIT_TIME)
         door_1.ChangeDutyCycle(CLOSED_CDC)
         door_2.ChangeDutyCycle(CLOSED_CDC)
         GPIO.cleanup()
@@ -48,28 +49,34 @@ class GateProcess:
         return True
 
     def getNFCUID(self):
-        def end_read():
-            GPIO.cleanup()
+        continue_reading = True
+
+        # Capture SIGINT for cleanup when the script is aborted
+        def end_read(uid):
+            global continue_reading
+            print "uID is: %s,%s,%s,%s" % (str(uid[0]), str(uid[1]), str(uid[2]), str(uid[3]))
+            continue_reading = False
 
         # Hook the SIGINT
         signal.signal(signal.SIGINT, end_read)
 
         # Create an object of the class MFRC522
         MIFAREReader = MFRC522.MFRC522()
+        while continue_reading:
+            # Scan for cards
+            (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-        # Scan for cards
-        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+            # If a card is found
+            if status == MIFAREReader.MI_OK:
+                print
+                "Card detected"
 
-        # If a card is found
-        if status == MIFAREReader.MI_OK:
-            print
-            "Card detected"
+            # Get the UID of the card
+            (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
-        # Get the UID of the card
-        (status, uid) = MIFAREReader.MFRC522_Anticoll()
-
-        # If we have the UID, return it and clean up.
-        if status == MIFAREReader.MI_OK:
-            return uid
-            end_read()
+            # If we have the UID, return it and clean up.
+            if status == MIFAREReader.MI_OK:
+                end_read(uid)
+                return uid
+               
 GateProcess()
