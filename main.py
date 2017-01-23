4432b78981437ba1cf582b229e6665652b51edb4
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2017-01-23 14:38:59 +0100

    code cleanup and compatibility

--- main.py
+++ main.py
@@ -14,21 +14,17 @@ YELLOW_LIGHT = 10
 RED_LIGHT = 12
 # Constants for PWM
 CLOSED_FREQ = 10
-CLOSED_CDC = 1.5
-OPEN_CCW = 0.8
-OPEN_CW = 2.1
-NOCHANGE = 0
+CLOSED = 1.5
+OPEN_CCW = 2.1
+OPEN_CW = 0.8
 WAIT_TIME = 0.5
 
-exitProcess = False
-
-
-class GateProcess:  # Process to open and close gates
+class GateProcess:  # Process to authenticate user, then open and close gates
     # global within gateProcess
     door_1 = None
     door_2 = None
 
-    def __init__(self):
+    def __init__(self, mode):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(SERVO_1, GPIO.OUT)
         GPIO.setup(SERVO_2, GPIO.OUT)
@@ -37,10 +33,15 @@ class GateProcess:  # Process to open and close gates
         GPIO.setup(RED_LIGHT, GPIO.OUT)
         global door_1
         global door_2
+        global OPEN_CCW
+        global OPEN_CW
+        if mode is 'ingang':
+            OPEN_CCW = 0.8
+            OPEN_CW = 2.1
         door_1 = GPIO.PWM(SERVO_1, CLOSED_FREQ)
         door_2 = GPIO.PWM(SERVO_2, CLOSED_FREQ)
-        door_1.start(CLOSED_CDC)
-        door_2.start(CLOSED_CDC)
+        door_1.start(CLOSED)
+        door_2.start(CLOSED)
         GPIO.output(RED_LIGHT, 1)
         uID = self.getNFCUID()
         if self.authenticated(uID):
@@ -55,12 +56,12 @@ class GateProcess:  # Process to open and close gates
         time.sleep(WAIT_TIME * 4)  # Wait 2 seconds
         GPIO.output(GREEN_LIGHT, 0)
         yellow_on = False
-        for i in range(0, 3, WAIT_TIME):  # For the next 3 seconds, flash yellow light
+        for i in range(0, 6):  # For the next 3 seconds, flash yellow light
             yellow_on = not yellow_on
             GPIO.output(YELLOW_LIGHT, yellow_on)
             time.sleep(WAIT_TIME)
-        door_1.ChangeDutyCycle(CLOSED_CDC)
-        door_2.ChangeDutyCycle(CLOSED_CDC)
+        door_1.ChangeDutyCycle(CLOSED)  # CLOSE THE GAAAATES!!!
+        door_2.ChangeDutyCycle(CLOSED)
         GPIO.output(RED_LIGHT, 1)
         time.sleep(WAIT_TIME)
         GPIO.cleanup()
@@ -99,4 +100,4 @@ class GateProcess:  # Process to open and close gates
                 end_read(uid)
                 return uid
                
-GateProcess()
+GateProcess('ingang')
