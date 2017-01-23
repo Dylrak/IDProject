2409a39ab0dc6b24ca0c09d789fad9fcd4b5cebe
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2017-01-23 12:35:32 +0100

    Testing threading

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
+import threading
 import time
 
-# constants
+# constants for GPIO(/PWM)
 SERVO_1 = 11
 SERVO_2 = 13
 CLOSED_FREQ = 10
@@ -10,34 +11,48 @@ OPEN_CCW = 0.8
 OPEN_CW = 2.1
 WAIT_TIME = 1
 
-# globals
-door_1 = None
-door_2 = None
+exitProcess = False
 
 
-def init():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(SERVO_1, GPIO.OUT)
-    GPIO.setup(SERVO_2, GPIO.OUT)
-    global door_1
-    global door_2
-    door_1 = GPIO.PWM(SERVO_1, CLOSED_FREQ)
-    door_2 = GPIO.PWM(SERVO_2, CLOSED_FREQ)
-    door_1.start(CLOSED_CDC)
-    door_2.start(CLOSED_CDC)
+class GateProcess(threading.Thread):
+    # global within gateProcess
+    door_1 = None
+    door_2 = None
 
+    def __init__(self):
+        threading.Thread.__init__(self)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(SERVO_1, GPIO.OUT)
+        GPIO.setup(SERVO_2, GPIO.OUT)
+        global door_1
+        global door_2
+        door_1 = GPIO.PWM(SERVO_1, CLOSED_FREQ)
+        door_2 = GPIO.PWM(SERVO_2, CLOSED_FREQ)
+        door_1.start(CLOSED_CDC)
+        door_2.start(CLOSED_CDC)
 
-def main():
-    init()
-    try:
-        while True:
+    def run(self):
+        while not exitProcess:
             time.sleep(WAIT_TIME)
             door_1.ChangeDutyCycle(OPEN_CCW)
             door_2.ChangeDutyCycle(OPEN_CW)
             time.sleep(WAIT_TIME)
             door_1.ChangeDutyCycle(CLOSED_CDC)
             door_2.ChangeDutyCycle(CLOSED_CDC)
-    except KeyboardInterrupt:
         GPIO.cleanup()
-main()
+        self.name.exit()
 
+
+class TestProcess(threading.Thread):
+    def __init(self):
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        try:
+            global exitProcess
+        except KeyboardInterrupt:
+            exitProcess = True
+gateThread = GateProcess()
+testThread = TestProcess()
+gateThread.start()
+testThread.start()
