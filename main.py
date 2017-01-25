3804b0c1b3c13be6e6291708d7d0e1b2e6bff18e
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2017-01-25 15:03:26 +0100

    MOAR BOOGFYX

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import Gates
 class GUI(Frame):
     def createMenu(self):
         button = \
-            Button(self, text="Ingang", command = lambda: Gates.GateProcess(True))\
-                .pack()
+            Button(self, text="Ingang", command = lambda: Gates.GateProcess(True))
+        button.pack()
         button.grid(row=0, column=0)
 
         button = \
-            Button(self, text = "Uitgang", command = lambda: Gates.GateProcess(False)) \
-                .pack()
+            Button(self, text = "Uitgang", command = lambda: Gates.GateProcess(False))
+        button.pack()
         button.grid(row=0, column=0).grid(row=0, column = 1)
 
         button = \
-            Button(self, text = "Registratie", command = lambda: Gates.GateProcess(False))\
-                .pack()
+            Button(self, text = "Registratie", command = lambda: Gates.GateProcess(False))
+        button.pack()
         button.grid(row=1, column = 0, rowspan=2)
 
     def __init__(self, master=None):
