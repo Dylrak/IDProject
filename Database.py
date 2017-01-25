d6f025591cb695ec1c06b76281eae0cca44d13f4
Dylrak-Pi <dylrak@hotmail.com>
2017-01-25 15:58:48 +0000

    Added brackets >_>

--- Database.py
+++ Database.py
@@ -7,7 +7,7 @@ def authenticate(uID):
     except:
         print("Cannot connect to the database!")
         return False
-    cur = conn.cursor
+    cur = conn.cursor()
     try:
         cur.execute("SELECT datuminschrijving FROM klantlidmaatschap WHERE nfcid=%s AND (datumuitschrijving = Null OR datumuitschrijving < current_date)", uID)  # Using this query,
         # any values returned will indicate that the user is authenticated.
@@ -15,23 +15,24 @@ def authenticate(uID):
         print("Cannot find open subscription!")
     authenticated = False
     try:
-        if cur.fetchone is not None:  # fetchone is a method used to fetch the results of the execute statement above.
+        if cur.fetchone() is not None:  # fetchone is a method used to fetch the results of the execute statement above.
             # It either returns the date of an active subscription, None when it's empty
             # and throws a ProgrammingError if the execute command found absolutely nothing.
             authenticated = True
     except psycopg2.ProgrammingError:
         print("Query returned no results, user is unknown.")
     finally:
-        conn.close
+        cur.close()
+        conn.close()
     return authenticated
 
 def addCustomer(data):
     try:
         conn=psycopg2.connect("dbname='Sportschool' user='postgres' host='192.168.1.2' password='omgidpomg'")
-        dat = conn.cursor
+        dat = conn.cursor()
         dat.execute("INSERT INTO klant (nfcid, iban, geboortedatum, straatnaam, huisnummer, plaats, postcode, voornaamklant, achternaamklant, emailadresklant) VALUES ", data)
         conn.commit()
-        conn.close
+        conn.close()
     except:
         print("Cannot connect to the database!")
         return False
